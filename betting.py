fbafa6e0cb78042cf7458d392996265c235992fe
PhilipUrhammar <phikip.urhanmar@elev.ga.dbgy.se>
2025-11-20 10:38:01 +0100

    publish

--- betting.py
+++ betting.py
@@ -1,67 +1,107 @@
 '''
-app.py: Ett betting system där du börjar med 1000 kr och du har 7 chanser att gissa fram ett tal mellan 1-100.
+betting.py: Ett betting system där du börjar med 1000 kr och du har 7 chanser att gissa fram ett tal mellan 1-100.
 
 __author__  = "Philip Urhammar"
 __version__ = "1.0.0"
 __email__   = "philip.urhammar@elev.ga.dbgy.se"
 '''
+
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    DEFAULT = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 # 1. Spelet ska själv slumpa ett tal (mellan 1-100) som är hemligt: random.randint(1, 100), man ska ha sju försök. Du får själv bestämma hur spelet ska se ut
 import os
 import random
-
+import time
 money = 1000 
 
 
 while True:
     os.system('cls')
-    print("__________        __    __  .__                                      .__               ")
-    print("\______   \ _____/  |__/  |_|__| ____    ____     ____ _____    _____|__| ____   ____  ")
-    print(" |    |  _// __ \   __\   __\  |/    \  / ___\  _/ ___\\__  \  /  ___/  |/    \ /  _ \ ")
-    print(" |    |   \  ___/|  |  |  | |  |   |  \/ /_/  > \  \___ / __ \_\___ \|  |   |  (  <_> )")
-    print(" |______  /\___  >__|  |__| |__|___|  /\___  /   \___  >____  /____  >__|___|  /\____/ ")
-    print("        \/     \/                   \//_____/        \/     \/     \/        \/        ")
+    print(bcolors.GREEN + f"__________        __    __  .__                                      .__               ")
+    print(bcolors.GREEN + f"\______   \ _____/  |__/  |_|__| ____    ____     ____ _____    _____|__| ____   ____  ")
+    print(bcolors.GREEN + f" |    |  _// __ \   __\   __\  |/    \  / ___\  _/ ___\\__  \  /  ___/  |/    \ /  _ \ ")
+    print(bcolors.GREEN + f" |    |   \  ___/|  |  |  | |  |   |  \/ /_/  > \  \___ / __ \_\___ \|  |   |  (  <_> )")
+    print(bcolors.GREEN + f" |______  /\___  >__|  |__| |__|___|  /\___  /   \___  >____  /____  >__|___|  /\____/ ")
+    print(bcolors.GREEN + f"        \/     \/                   \//_____/        \/     \/     \/        \/        ")
     
     randomized = random.randint(1, 100)
-    print(f"Du har {money} kr i saldot, du ska gissa rätt på ett tal mellan 1-100, du har sju försök")
+    print(bcolors.BOLD + f"Du har {money} kr i saldot, du ska gissa rätt på ett tal mellan 1-100, du har sju försök")
 
     while True:
         try:
-            bet = int(input("Hur mycket vill du betta?: "))      
+            bet = int(input(bcolors.GREEN + f"Hur mycket vill du betta?: "))      
             if bet > money:
-                print("Du kan inte betta mer än vad du har på saldot, du har", money ,"på kontot")
+                print(bcolors.RED + f"Du kan inte betta mer än vad du har på saldot, du har", money ,"på kontot")
             elif bet <= 0:
-                print("Bet måste vara ett positivt tal, försök igen.")
+                print(bcolors.RED + f"Bet måste vara ett positivt tal, försök igen.")
             else:
                 break
         except ValueError:
-            print("Felaktig inmatning, skriv en siffra.")
-
+            print(bcolors.RED + f"Felaktig inmatning, skriv en siffra.")
+            
     won = False
-    for attempt in range(1, 8):
+    attempt = 1
+    while attempt < 8:
         try:
+            print("-------------------------------")
             guess = int(input(f"Gissning {attempt}: "))
         except ValueError:
-            print("Felaktig inmatning, försök igen.")
+            print(bcolors.RED + f"-------------------------------")
+            print(bcolors.RED + f"Felaktig inmatning, försök igen.")
             continue
+        
+        if not 1 <= guess <= 100:
+            print(bcolors.RED + f"-------------------------------")
+            print(bcolors.RED + f"Din gissning får inte vara mindre än 1 eller större än 100")
+            continue
+            
         if guess > randomized:
-            print("Din gissning är större än talet")
+            print(bcolors.RED + f"För stort")
+            attempt += 1
         elif guess < randomized:
-            print("Din gissning är mindre än talet")
+            print(bcolors.BLUE + f"För litet")
+            attempt += 1
         else:
-            print("Grattis! Du gissade rätt!")
-            money += bet
-            print("Grattis, du har nu ", money ,"kr")
+            print(bcolors.GREEN + f"-------------------------------")
+            print(bcolors.GREEN + f"Grattis! Du gissade rätt!")
+            money += bet*2
+            print("Du har nu ", money ,"kr på kontot")
             won = True
             break
+            
     if not won:
         money -= bet
-        print(f"Tyvärr, du gissade fel. Du har nu {money} kr")
+        print(bcolors.RED + f"--------------------------------------------------------------------------")
+        print(bcolors.RED + f"Tyvärr, du gissade fel. Talet var {randomized}. Du har nu {money} kr")
 
-    restart = input("Spela igen? (Enter = Ja, N = Nej)")
-    if restart == "n":
-        print("Tack för att du spelade!")
+    if money <= 0:
+        print(bcolors.RED + f"Du kan inte spela igen då du har 0 kr på saldot. Tack för att du spelade!")
+        time.sleep(2)
+        os.system('cls')
+        break 
+    
+    while True:
+        restart = input(bcolors.GREEN + f"Spela igen? (J = Ja, N = Nej): ").upper()
+        if restart == "N":
+            print(bcolors.GREEN + f"Tack för att du spelade!")
+            time.sleep(2)
+            os.system('cls')
+            break
+        elif restart == "J":
+            break
+        else:
+            print(bcolors.RED + f"Ogiltigt val. Svara med J eller N.")
+            
+    if restart == "N":
         break
-    else:
-        continue
 
 # 8. Dokumentera hur det gick och vad du kunde göra bättre
